1500f81c3b2b62c4e81c660dffa164a03e1ce4c3
AugustoMagalhaes <augustommg@gmail.com>
2022-06-21 17:26:37 -0300

    chore: adequa mensagens e condiçoes de validaçao de email.

--- middlewares/validateEmail.js
+++ middlewares/validateEmail.js
@@ -3,8 +3,11 @@ const validateEmail = (req, res, next) => {
   const emailRegex = new RegExp(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i);
   const testValidation = emailRegex.test(email);
   // fonte: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
   if (!testValidation) {
-    return res.status(400).json({ message: 'Invalid email!' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   next();
 };

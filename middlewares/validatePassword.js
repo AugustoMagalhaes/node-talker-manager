97085ade2b1c86d3e5e6afdb147fb355852ed880
AugustoMagalhaes <augustommg@gmail.com>
2022-06-21 17:32:51 -0300

    chore: cria middleware para validaçao de password.

---
+++ middlewares/validatePassword.js
@@ -0,0 +1,13 @@
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (!password.length || password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+module.exports = validatePassword;

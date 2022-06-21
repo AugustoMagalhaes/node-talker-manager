250e71d4978f35ff635404f3820190624e1bdca0
AugustoMagalhaes <augustommg@gmail.com>
2022-06-21 18:00:34 -0300

    chore: cria estrutura da requisiçao POST de 'talker'.

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 const talkerRouter = express.Router();
 
 const mainRead = require('../helpers/mainRead');
+const mainWrite = require('../helpers/mainWrite');
 
 const PATH = 'talker.json';
 
@@ -17,6 +18,14 @@ talkerRouter.get('/:id', async (req, res) => {
   const foundTalker = data.find((el) => el.id === Number(id));
   if (foundTalker) return res.status(200).json(foundTalker);
   return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
+talkerRouter.post('/', async (req, res) => {
+  const newData = req.body;
+  const newTalkers = await mainWrite(PATH, newData);
+  const test = await mainRead(PATH);
+  console.log(test);
+  return res.status(201).json(newTalkers);
 });
 
 module.exports = talkerRouter;

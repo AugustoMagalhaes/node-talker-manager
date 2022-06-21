4f157ad471beffc17052f3b2e84ea7e9bb290b19
AugustoMagalhaes <augustommg@gmail.com>
2022-06-21 14:24:22 -0300

    chore: implementa rota POST 'talker/:id' padrão.

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -11,4 +11,12 @@ talkerRouter.get('/', async (req, res) => {
   return res.status(200).json(data);
 });
 
+talkerRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await mainRead(PATH);
+  const foundTalker = data.find((el) => el.id === Number(id));
+  if (foundTalker) return res.status(200).json(foundTalker);
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
 module.exports = talkerRouter;

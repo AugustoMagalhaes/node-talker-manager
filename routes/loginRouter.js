90bed11e9e93136f3891218867177245bbfa11ed
AugustoMagalhaes <augustommg@gmail.com>
2022-06-21 17:46:34 -0300

    chore: implementa os middlewares de validaçao de email e password no loginRouter.

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -3,8 +3,10 @@ const express = require('express');
 const loginRouter = express.Router();
 
 const generateToken = require('../helpers/generateToken');
+const validateEmail = require('../middlewares/validateEmail');
+const validatePassword = require('../middlewares/validatePassword');
 
-loginRouter.post('/', async (req, res) => {
+loginRouter.post('/', validateEmail, validatePassword, async (req, res) => {
   const token = generateToken(16);
   return res.status(200).json({ token });
 });

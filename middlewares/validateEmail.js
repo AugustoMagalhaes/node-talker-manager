42c5a944b7120ef94c86217dcfaaea79050d846e
AugustoMagalhaes <augustommg@gmail.com>
2022-06-21 17:45:05 -0300

    chore: corrige regExp que falhava ao usar modelo correto de email.

--- middlewares/validateEmail.js
+++ middlewares/validateEmail.js
@@ -1,6 +1,6 @@
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
-  const emailRegex = new RegExp(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i);
+  const emailRegex = new RegExp(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+/i);
   const testValidation = emailRegex.test(email);
   // fonte: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
   

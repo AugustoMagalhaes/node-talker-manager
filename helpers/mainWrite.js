b726cf662746ab414b7cbf2b64235395416e1cf8
AugustoMagalhaes <augustommg@gmail.com>
2022-06-22 17:24:24 -0300

    chore: adequa mainWrite para atuar de forma generica.

--- helpers/mainWrite.js
+++ helpers/mainWrite.js
@@ -1,15 +1,16 @@
 const fs = require('fs').promises;
-const mainRead = require('./mainRead');
 
 const mainWrite = async (PATH, newData) => {
   try {
-    const data = await mainRead(PATH);
-    const appendedData = [...data, newData];
-    const striginfiedData = JSON.stringify(appendedData);
+    const striginfiedData = JSON.stringify(newData);
     await fs.writeFile(PATH, striginfiedData);
   } catch (err) {
     throw new Error(`Impossible to write in ${PATH}`);
   }
 };
 
-module.exports = mainWrite;
+module.exports = mainWrite; 
+
+/* const data = await mainRead(PATH);
+    const appendedData = [...data, newData];
+    const striginfiedData = JSON.stringify(appendedData); */

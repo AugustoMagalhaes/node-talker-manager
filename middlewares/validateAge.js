a1484b4490e3eed2bc015e28b32f143e5d89a660
AugustoMagalhaes <augustommg@gmail.com>
2022-06-22 00:45:23 -0300

    chore: cria middleware de validaçao de idade; add corretamente o de idade.

--- middlewares/validateAge.js
+++ middlewares/validateAge.js
@@ -1,4 +1,13 @@
 const validateAge = (req, res, next) => {
   const { age } = req.body;
+
+  if (!Number.isInteger(age) || !age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
   next();
 };
+
+module.exports = validateAge;
